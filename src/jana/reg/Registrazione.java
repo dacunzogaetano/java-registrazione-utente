e2ea305fec4ab529db37f2c108fd72871cf1a6f0
dacunzogaetano <dacunzogaetano@gmail.com>
2022-06-24 15:47:30 +0200

    esercizio completo

--- src/jana/reg/Registrazione.java
+++ src/jana/reg/Registrazione.java
@@ -11,32 +11,37 @@ public class Registrazione {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		try {
-			System.out.println("Inserisci il tuo nome");
-			String nome = scan.nextLine();
+		
+		boolean ciclo = true; 
+		
+		do {
+			try {
+				System.out.println("Inserisci il tuo nome");
+				String nome = scan.nextLine();
 			
-			System.out.println("Inserisci il tuo cognome");
-			String cognome = scan.nextLine();
+				System.out.println("Inserisci il tuo cognome");
+				String cognome = scan.nextLine();
 			
-			System.out.println("Inserisci una email che contenga almeno un . ed una @");
-			String email = scan.nextLine();
+				System.out.println("Inserisci una email che contenga almeno un . ed una @");
+				String email = scan.nextLine();
 			
-			System.out.println("Inserisci una password compresa tra 8 e 12 caratteri");
-			String password = scan.nextLine();
+				System.out.println("Inserisci una password compresa tra 8 e 12 caratteri");
+				String password = scan.nextLine();
 			
-			System.out.println("Inserisci la tua età, che deve essere di almeno 18 anni");
-			int eta = Integer.parseInt(scan.nextLine());
+				System.out.println("Inserisci la tua età, che deve essere di almeno 18 anni");
+				int eta = Integer.parseInt(scan.nextLine());
 			
-			Utente prova = new Utente(nome, cognome, email, password, eta);
+				Utente prova = new Utente(nome, cognome, email, password, eta);
 			
-			System.out.println(prova.toString());
+				System.out.println(prova.toString());
+				ciclo = false;
 			
-		} catch (Exception e) {
-			System.out.println("Impossibile creare l'utente");
-		      System.out.println(e.getMessage());
-			
-		      
-		}
+			    }  
+			catch (Exception e) {
+				System.out.println("Impossibile creare l'utente");
+				System.out.println(e.getMessage());
+				}     
+		  } while (ciclo);
 
 		 scan.close();
 	}
